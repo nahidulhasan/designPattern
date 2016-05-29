1560e0e75f5ec86334cd287aa6830766ac0707bd
nahid35 <nahidul.cse@gmail.com>
2016-05-29 10:51:21 +0600

    stratedy pattern

---
+++ strategy.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Define a family of algorithms, encapsulate each one, and make them interchangeable.
+ * Strategy lets the algorithm vary independently from clients that use it.
+ */
+
+interface Logger{
+
+    public function log($data);
+}
+
+
+// Define a family of algorithm
+
+class LogToFile implements Logger{
+
+    public function log($data)
+    {
+
+        var_dump('Log the data to a file');
+    }
+}
+
+class LogToDatabase implements Logger{
+
+    public function log($data)
+    {
+
+        var_dump('Log the data to database');
+    }
+}
+
+class LogToXWebService implements Logger{
+
+    public function log($data)
+    {
+        var_dump('Log the data to a Saas site');
+    }
+}
+
+
+class App {
+
+    public function log($data, Logger $logger = null){
+
+        $logger = $logger ? : new LogToFile();
+        $logger->log($data);
+    }
+}
+
+
+$app = new App();
+
+$app->log('Some information from here');
+
+$app->log('Some information from here', new LogToDatabase());
