478f25a597fa1e7e327ef26ce3a10774b3017e67
Nahidul Hasan <nahid35@users.noreply.github.com>
2017-07-09 11:25:48 +0600

    Update decorator.php
    Modify decorator pattern

--- decorator.php
+++ decorator.php
@@ -1,5 +1,10 @@
 <?php
 
+ /*
+ * Allows behavior to be added to an individual object,
+ *  either statically or dynamically, without affecting the behavior of other objects from the same class.
+ */
+
 interface eMailBody{
     public function loadBody();
 }
